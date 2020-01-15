ed77232edf301e929883218fc57478ea42b8590c
rj8228 <rj8228@gmail.com>
2020-01-15 12:44:20 +0530

    complete templating done

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup'),
+    path('launchpad',views.launchpad,name='launchpad'),
+    path('upload',views.upload,name='upload'),
+    path('cluster',views.cluster,name='cluster'),
+    path('visualize',views.visualize,name='visualize'),
 ]
