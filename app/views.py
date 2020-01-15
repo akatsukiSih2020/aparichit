29764f7e7ac7444008ec6bd93a88421c708186d4
rj8228 <rj8228@gmail.com>
2020-01-15 14:43:30 +0530

    login written

--- app/views.py
+++ app/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login,logout
 from django.http import HttpResponse,JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
-def home(request):
+def hom(request):
     return render(request,'app/home.html')
-def login(request):
+def log(request):
     if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            pass
+        # return render(request,'app/home.html')
+            return JsonResponse({'success': 'true'})
+def logo(request):
+    if request.method == 'POST':
+        logout(request)
         return render(request,'app/home.html')
 
 def signup(request):
     if request.method == 'POST':
+        print( request.POST['username'])
+        us=User.objects.create_user( request.POST['username'],  request.POST['email'],  request.POST['password'])
+        us.save()  
         return render(request,'app/home.html')
 
-def launchpad(request):
+def launch(request):
     if request.method == 'GET':
         return render(request,'app/launchpad.html')
     elif request.method == 'POST':
