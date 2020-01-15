8b24b3cfa1464f167e4175985c5221affff9a333
rj8228 <rj8228@gmail.com>
2020-01-15 09:06:36 +0530

    templating done

--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello, this is home.")
-    return render(request,'home.html')
+    return render(request,'app/home.html')
