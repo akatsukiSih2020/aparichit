8fc594f8aaaed8def0ff8c982d9e3baf9db6af10
dheerajodha <dheerajsinghjodha@yahoo.com.au>
2020-01-15 17:22:26 +0530

    Adds upload and launchpad

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login',views.login,name='login'),
-    path('signup',views.signup,name='signup'),
-    path('launchpad',views.launchpad,name='launchpad'),
-    path('upload',views.upload,name='upload'),
-    path('cluster',views.cluster,name='cluster'),
-    path('visualize',views.visualize,name='visualize'),
+    path('/login',views.login,name='login'),
+    path('/signup',views.signup,name='signup'),
+    path('/launchpad/new_lpd',views.new_lpd,name='new_lpd'),
+    path('/launchpad',views.launchpad,name='launchpad'),
+    path('/upload/new_file',views.new_file,name='new_file'),
+    path('/upload',views.upload,name='upload'),
+    path('/cluster',views.cluster,name='cluster'),
+    path('/visualize',views.visualize,name='visualize'),
 ]
+
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
 # Create your views here.
 def home(request):
@@ -31,4 +31,10 @@ def visualize(request):
         return render(request,'app/visualize.html')
     elif request.method == 'POST':
         return JsonResponse({'success': 'true'})
+
+def new_lpd(request):
+    return render(request,'app/new_lpd.html')
     
+def new_file(request):
+    return render(request,'app/new_file.html') 
+
