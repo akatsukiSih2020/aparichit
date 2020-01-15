391486544097b505eaf8b921abd64ebb57262526
rj8228 <rj8228@gmail.com>
2020-01-15 23:27:47 +0530

    db added

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ class data(models.Model):
     inputfile_path=models.CharField(max_length=50)
     processedfile_path=models.CharField(max_length=50,blank=True)
     prediction=models.CharField(max_length=30,blank=True)
+class launchpad(models.Model):
+    user=models.CharField(max_length=30)
+    latitude=models.FloatField()
+    longitude=models.FloatField()
+    altitude=models.FloatField()
