52e5a57ea266efb0c54e83da9cddea019551ef49
dheerajodha <dheerajsinghjodha@yahoo.com.au>
2020-01-16 08:18:09 +0530

    side navigation toggle

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -1,10 +1,12 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "0px";
+    document.getElementById("full_body").style.marginLeft = "150px";
 }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("full_body").style.marginLeft = "0";
 }
