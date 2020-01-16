bbf28a25b28fefc16f9150a63144ce231d6ec677
dheerajodha <dheerajsinghjodha@yahoo.com.au>
2020-01-16 13:23:42 +0530

    Adds js for file upload

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -10,3 +10,27 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("full_body").style.marginLeft = "0";
 }
+
+
+// $("#file-picker").change(
+
+  function fileUpload(){
+
+        var input = document.getElementById('file-picker');
+
+        for (var i=0; i<input.files.length; i++)
+        {
+        //koala.jpg, koala.JPG substring(index) lastIndexOf('a') koala.1.jpg
+            var ext= input.files[i].name.substring(input.files[i].name.lastIndexOf('.')+1).toLowerCase()
+        
+        if (ext == 'csv') 
+            {
+                $("#msg").text("Files are supported")
+            }
+            else
+            {
+                $("#msg").text("Files are NOT supported")
+                document.getElementById("file-picker").value ="";
+            }
+        }
+    }
